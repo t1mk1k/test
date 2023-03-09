27b3f394607bf05f9c1d6c03f5e6e08af42098fe
t1mk1k <96469032+t1mk1k@users.noreply.github.com>
2023-03-09 09:59:37 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,29 @@
-print("Hello world!")
+#!/usr/bin/python
+
+import argparse
+
+
+if __name__ == '__main__':
+    '''
+    script for preprocessing books and extracting valence
+    example use: python run.py -i './data/raw/' -o './data/emotion_data' -t './data/tmp'
+    
+    '''
+    print("Starting Python script in workspace")
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input_path', help='input folder', required=True)
+    parser.add_argument('-o', '--output_path', help='output folder', required=True)
+    parser.add_argument('-t', '--temp', help='temp folder', required=True)
+
+    data_file_path = f"{args.input_path}/words.txt"
+    results_file_path = f"{args.output_path)/results.txt"
+    
+    with open(data_file_path, "r") as data_file:
+      data = data_file.read()
+      words = data.split()
+
+    average_word_length = reduce(lambda x, y: x+y, words, 0) / float(len(words))
+
+    with open(results_file_path, "w") as results_file:
+      results_file.write(f"Average word length is {average_word_length}"
